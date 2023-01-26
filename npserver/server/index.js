ea7f46dbe6ae873725b585465dc7230c967ebae1
Connor Fung <cmfung23@gmail.com>
2023-01-25 16:24:31 -0800

    to get all parks and parksByState api routes

--- npserver/server/index.js
+++ npserver/server/index.js
@@ -1,5 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const controllers = require('./controllers/controllers');
 
 const PORT = process.env.PORT || 4001;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/parks', controllers.getParks);
+app.get('/stateParks/:state', controllers.getParksByState);
+app.listen(PORT, () => { console.log(`Listening at http://localhost:${PORT}`); });
